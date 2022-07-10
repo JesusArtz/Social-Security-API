73d6289af483e06eece9acae35440c7eb30db938
Jesus Artz <realjesusartz@gmail.com>
2022-07-10 01:09:44 -0500

    Working

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from crypt import methods
+import json
 from methods.genUUID import GenerateUUID
-from flask import Flask, request
+from flask import Flask, jsonify, request
 import flask_cors
 from werkzeug.security import generate_password_hash as genph
 from werkzeug.security import check_password_hash as checkph
+import models as md
 
 app = Flask(__name__)
 flask_cors.CORS(app)
-app.secret_key(GenerateUUID())
+
 
 
 @app.route('/api/v1/users/register', methods=['POST'])
@@ -23,5 +25,96 @@ def Register():
         
         if password and email and username:
             
+            hPass = genph(password)
             
+            responseModel = md.registerUser(email, username, hPass)
+            
+            if responseModel:
+                
+                getHashedPass = md.getHash(email)
+                
+                unHashPass = checkph(getHashedPass[0], password)
+                
+                if unHashPass:
+                    
+                    respLogin = md.loginUser(email, getHashedPass[0])
+                    
+                    if respLogin:
+                        
+                        return jsonify(respLogin)
+                    
+                return jsonify({'response':'an error as ocurred 1'})
+            
+            return jsonify({'response':'an error as ocurred 2'})
+        
+        return jsonify({'response':'an error as ocurred 3'})
+    
+                      
+@app.route('/api/v1/users/login', methods=['POST'])  
+def Login():
+    
+    if request.method == 'POST':
+        
+        data = request.get_json()
+        
+        if data:
+            
+            email = data['email']
+            password = data['password']
+            
+            if email and password:
+                
+                getHashedPass = md.getHash(email)
+                
+                if getHashedPass:
+                
+                    unHashPass = checkph(getHashedPass[0], password)
+                    
+                    if unHashPass:
+                        
+                        respLogin = md.loginUser(email, getHashedPass[0])
+                        
+                        if respLogin:
+                            
+                            getToken = f"{GenerateUUID()}"
+                            
+                            respSession = md.generateSession(respLogin['id'], getToken)
+                            
+                            if respSession:
+                                
+                                return jsonify({
+                                    "id":respLogin['id'],
+                                    "username":respLogin['username'],
+                                    "email":respLogin['email'],
+                                    "token":f"{getToken}"
+                                })
+                
+                    return jsonify({'response':'Incorrect Password'})                                    
+                
+                return jsonify({'response':'Incorrect Email'})  
+                
+            return jsonify({'response':'Null data'})  
+        
+        return jsonify({'response':'No data'})  
 
+@app.route('/api/v1/users/logout', methods=['POST'])
+def Logout():
+    
+    if request.method == 'POST':
+        
+        data = request.get_json()
+        
+        if data:
+            
+            id = data['id']
+            
+            if id:
+                
+                respLogout = md.deleteSession(id)
+                
+                if respLogout:
+                    
+                    return jsonify({})
+
+
+app.run(port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,104 @@
-import 
+import sqlite3
+from urllib import response
+
+def conection():
+    
+    con = sqlite3.connect('database.db')
+    
+    return con
+
+
+
+
+
+def registerUser(email, username, password):
+    
+    con = conection()
+    
+    cursor = con.cursor()
+    
+    query = "INSERT INTO Users(email, username, password) VALUES ('%s', '%s', '%s')" %(email, username, password)
+    
+    cursor.execute(query)
+    
+    con.commit()
+    
+    return True
+
+def loginUser(email, password):
+    
+    con = conection()
+    
+    cursor = con.cursor()
+    
+    query = "SELECT id, username, email FROM Users WHERE email = '%s' AND password = '%s'" %(email, password)
+    
+    cursor.execute(query)
+    
+    resp = cursor.fetchall()
+    
+    if resp:
+        
+        return {
+            "id":resp[0][0],
+            "username":resp[0][1],
+            "email":resp[0][2]
+        }
+        
+    return False
+
+
+def generateSession(id, token):
+    
+    con = conection()
+
+    cursor = con.cursor()
+    
+    queryValidate = "SELECT token FROM Sessions WHERE user_id = '%s'" %id
+    
+    cursor.execute(queryValidate)
+    
+    respValidation = cursor.fetchone()
+    
+    if respValidation:
+        
+        return False
+    
+    query = "INSERT INTO Sessions (user_id, token) VALUES ('%s', '%s')" %(id, token)
+
+    cursor.execute(query)
+    
+    con.commit()
+    
+    return True
+
+def deleteSession(id):
+    
+    con = conection()
+    
+    cursor = con.cursor()
+    
+    query = "DELETE FROM Sessions WHERE user_id = '%s'" %id
+    
+    cursor.execute(query)
+    
+    con.commit()
+
+    return True
+
+
+def getHash(email):
+    
+    con = conection()
+    
+    cursor = con.cursor()
+    
+    query = "SELECT password FROM Users WHERE email = '%s'" %email
+    
+    cursor.execute(query)
+    
+    resp = cursor.fetchone()
+    
+    return resp
+
+
